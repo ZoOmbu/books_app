c5b1116dd61eb69d1644005b4cb9e5e207bce8e9
ZoOmbu <valerii.bondarenko@nure.ua>
2016-04-27 09:29:56 +0300

    paperclip completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -78,6 +78,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :image_url, :description, :genre, :author, :draft)
+      params.require(:book).permit(:title, :image_url, :description, :genre, :author, :draft,)
     end
 end
